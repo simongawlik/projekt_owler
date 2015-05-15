1c9948035d0c6ab65f70256214aadef68c0cd79c
Simon Gawlik <sgawlik@gmail.com>
2015-05-15 12:41:24 -0400

    finished problem 5 and 6

---
+++ problems/problem0x005.py
@@ -0,0 +1,59 @@
+# python 3
+# (C) Simon Gawlik
+# started 8/1/2015
+
+n = 15          #232792560
+primes = []
+non_primes = []
+
+# find prime numbers up to n
+def primes_lt_n (n):
+    for num in range (2, n + 1):
+        is_prime = True
+        for i in range (2, num):
+            if num % i == 0:
+                is_prime = False
+        if is_prime:
+            primes.append(num)
+        else:
+            non_primes.append(num)
+
+primes_lt_n (n)
+print (primes)
+print (non_primes)
+
+def get_max_product (number):
+    if number <= 2:
+        return (number)
+    else:
+        max_product = 1
+        for counter in range (1, number + 1):
+            max_product = max_product * counter
+        return (max_product)
+
+# tests
+assert get_max_product (0) == 0
+assert get_max_product (1) == 1
+assert get_max_product (2) == 2
+assert get_max_product (3) == 6
+assert get_max_product (10) == 3628800
+assert get_max_product (11) == 39916800
+
+max_product_n = get_max_product (n)
+
+# check if a number is divisible by all the numbers in a given list
+def has_divisors_in (number, lst):
+    for member in lst:
+        if number % member != 0:
+            return (False)
+            break
+    return (True)
+
+# main
+for counter in range (n, max_product_n):
+    if (has_divisors_in (counter, primes) and       # check for primes first
+        has_divisors_in (counter, non_primes)):
+        print (counter)
+        break
+    
+
---
+++ problems/problem0x006.py
@@ -0,0 +1,22 @@
+# python 3
+# (C) Simon Gawlik
+# started 8/2/2015
+
+# OCaml, baby!
+import functools
+
+
+n = 100
+
+# sum of squares
+squares = list(map(lambda x: x**2, range(1, n + 1)))
+print (squares)
+sum_of_squares = functools.reduce(lambda x, y: x + y, squares)
+
+# square of sum
+sum = functools.reduce(lambda x, y: x + y, range (1, n + 1))
+print (sum)
+square_of_sum = sum ** 2
+
+#result
+print (square_of_sum - sum_of_squares)
